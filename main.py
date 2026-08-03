fcb6d58ed0a33403ebb3190e9b5e07c50d260f34
auranissrina-pixel <auranissrina@gmail.com>
2026-08-03 17:29:18 +0700

    Create main.py

---
+++ main.py
@@ -0,0 +1,5 @@
+panjang = float(input("Masukkan panjang persegi panjang: "))
+lebar = float(input("Masukkan lebar persegi panjang: "))
+luas = panjang * lebar
+
+print("luas persegi panjang adalah:", luas)
