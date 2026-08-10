7218ec23829573d93e161ca5818d1b643d5c2248
auranissrina-pixel <auranissrina@gmail.com>
2026-08-10 14:16:32 +0700

    Add loop for continuous number input and check

--- 05_latihan_python.py
+++ 05_latihan_python.py
@@ -10,3 +10,17 @@ try:
 
 except ValueError:
     print("[Kesalahan] Harap masukkan angka bulat yang valid!")
+
+while True:
+    angka = input("Masukkan sebuah angka tekan \"q\" untuk keluar: ")
+    
+    if angka == "q":
+        break
+
+    angka = int(angka)
+
+    if angka % 2 == 0:
+        print(f"Angka {angka} adalah bilangan GENAP.")
+    else:
+        print(f"Angka {angka} adalah bilangan GANJIL.")
+
